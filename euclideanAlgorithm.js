6590278e851b8998eb82d76ff45a0ae2be09cc07
Juan Villela <juan@villela.co>
2020-08-07 18:50:18 -0400

    Added euclidean helper algorithms.

---
+++ euclideanAlgorithm.js
@@ -0,0 +1,19 @@
+/**
+ * Recursive version of Euclidean Algorithm of finding greatest common divisor (GCD).
+ *
+ * @param {number} originalA
+ * @param {number} originalB
+ *
+ * @return {number}
+ */
+const euclideanAlgorithm = (originalA, originalB) => {
+  // Make input numbers positive.
+  const a = Math.abs(originalA);
+  const b = Math.abs(originalB);
+
+  // To make algorithm work faster instead of subtracting one number from the other
+  // we may use modulo operation.
+  return b === 0 ? a : euclideanAlgorithm(b, a % b);
+};
+
+module.exports = euclideanAlgorithm;
