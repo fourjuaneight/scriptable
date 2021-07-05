dcb1033d3350d98f77476853356bbdac4690307c
Juan Villela <juan@villela.co>
2021-07-05 12:26:22 -0400

    Backup for 2021-07-05

--- destructObj.js
+++ destructObj.js
@@ -5,16 +5,37 @@ const toCapitalized = importModule("toCapitalized.js");
 
 const params = args.shortcutParameter;
 const cap = args.plainTexts[0];
+const keyVal = args.plainTexts[1];
 
 const keys = Object.keys(params).sort();
 const values = Object.values(params);
-let results = { keys, values };
+let results = {};
 
-if (Boolean(cap)) {
-  results = {
-    keys: keys.map(key => toCapitalized(key)),
-    values: values.map(value => toCapitalized(value))
-  }
+switch (true) {
+  case Boolean(cap) && Boolean(keyVal):
+    results = {
+      keys: keys.map((key) => `${toCapitalized(key)} - ${params[key] ? '✅' : '🚫'}`),
+      values: values.map((value) => toCapitalized(value)),
+    };
+    break;
+  case Boolean(cap):
+    results = {
+      keys: keys.map((key) => toCapitalized(key)),
+      values: values.map((value) => toCapitalized(value)),
+    };
+    break;
+  case Boolean(keyVal):
+    results = {
+      keys: keys.map((key) => `${key} - ${params[keys]}`),
+      values,
+    };
+    break;
+  default:
+    results = {
+      keys,
+      values,
+    };
+    break;
 }
 
 Script.setShortcutOutput(results);
